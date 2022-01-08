e84b44858ec12943074dcdf8b348b8d82e0f581f
monaqa <cmonaqa@gmail.com>
2022-01-08 15:19:00 +0900

    clear warnings

--- bindings/rust/main.rs
+++ bindings/rust/main.rs
@@ -1,8 +1,6 @@
-use std::{fmt::Display, path::PathBuf};
+use std::path::PathBuf;
 
 use anyhow::*;
-use itertools::Itertools;
-use tree_sitter::{Parser, TreeCursor};
 
 use structopt::StructOpt;
 use tree_sitter_todome::syntax::ast::{AstNode, SourceFile};
@@ -14,10 +12,6 @@ struct Opts {
 
 fn main() -> Result<()> {
     let opts = Opts::from_args();
-
-    let language_todome = tree_sitter_todome::language();
-    let mut parser = Parser::new();
-    parser.set_language(language_todome)?;
 
     let text = std::fs::read_to_string(opts.input).context(anyhow!("The input file not found!"))?;
     let source_file = SourceFile::parse(text)?;
