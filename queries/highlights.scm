f88c20be98fd8bd561e0398a42737cb8e4b3f8ad
monaqa <cmonaqa@gmail.com>
2022-01-15 13:09:49 +0900

    fix & improve query (highlights)

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,4 +1,4 @@
-[ (comment) ] @string
+[ (memo) ] @string
 
 ((category) @keyword)
 
@@ -11,12 +11,18 @@
 ((tag) @tag.attribute)
 
 (task (status [(status_done) (status_cancel)]) ) @comment
-(task (status [(status_todo) (status_doing)]) (text) @none )
 (header (status [(status_done) (status_cancel)]) ) @comment
-(header (status [(status_todo) (status_doing)]) children: (_) @none)
 
 (task status: (status [(status_todo) (status_doing)])) @none
 (header status: (status [(status_todo) (status_doing)])) @none
+; (task (status [(status_todo) (status_doing)]) (text) @none )
+; (header (status [(status_todo) (status_doing)]) children: (_) @none)
+
+[
+ (status_todo)
+ (status_doing)
+ (status_other)
+ ] @punctuation.delimiter
 
 (
  [
